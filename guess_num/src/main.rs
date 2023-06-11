558c55377511d7cac65c9cf46ad95fe10e64ec5a
huchanghao <2955568032@qq.com>
2023-06-11 17:09:53 +0800

    01_guess_num

--- guess_num/src/main.rs
+++ guess_num/src/main.rs
@@ -1,4 +1,8 @@
+use std::io;
+
 fn main() {
     println!("猜数！");
     let mut guess_num = String::new();
+    io::stdin().read_line(& mut  guess_num).expect("无法读取行");
+    println!("你猜的数是{}",guess_num);
 }
