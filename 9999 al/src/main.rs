e7ad1e1c93afe09342eac05c44b87edd6ef62660
huchanghao <2955568032@qq.com>
2024-04-09 01:20:22 +0800

    learning

--- 9999 al/src/main.rs
+++ 9999 al/src/main.rs
@@ -1,23 +1,13 @@
-#[derive(Debug)]
-struct Point {
-    x:i32,
-    y:i32
-}
-
-impl From<[i32; 2]> for Point {      // 实现从 [i32; 2] 到 Point 的转换
-    fn from([x, y]: [i32; 2]) -> Self {
-        Point { x, y }
-    }
-}
 fn main() {
-    let p1 = Point::from([1,2]);
-    let p2= Point::from([1,2]);
+    // 这里 Vec<T> 在调用 iter() 时被解引用成 &[T]，所以可以访问 iter()
+    let v1 = vec![1, 2, 3, 4];
+    let result = 
+    v1.into_iter()
+        .map(|v| v * v)
+        .filter(|v| *v < 16)
+        .take(1)
+        .collect::<Vec<_>>();
 
-    let mut list:Vec<_> = Vec::new();
-    list.push(p1);
-    list.push(p2);
-    let mut list2 = list.into_iter()
-    .map(|point| (Point::from([point.x,point.y])))
-    .collect::<Vec<Point>>();
-    println!("{:?}",list)
-} 
+    println!("{:?}", result);
+    println!("{:?}", v1);
+}
